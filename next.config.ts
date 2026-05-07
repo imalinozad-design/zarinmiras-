60697f5a25e58af44056bfbcda4c4b402e50cd39
imalinozad-design <imalinozad@gmail.com>
2026-05-08 01:07:27 +0330

    feat: static export for Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  devIndicators: false,
   typescript: {
     ignoreBuildErrors: true,
   },
